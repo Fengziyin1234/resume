33b8b90c39f74e0f1fa50a742a55bb858d0bcdc2
Isaac Feng <zfeng5@stevens.edu>
2016-10-05 12:26:51 -0400

    Homepage finished

---
+++ src/routes.js
@@ -0,0 +1,45 @@
+(function() {
+    'use strict'
+
+    angular.module("ResumeApp")
+        .config(RoutesConfig);
+
+
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+    	console.log("hello");
+    	$urlRouterProvider.otherwise('/');
+
+    	 $stateProvider
+
+    	 .state('home', {
+    	 	url:'/',
+    	 	templateUrl: 'src/templates/home.html'
+    	 })
+
+    	 // education part
+    	 .state('education', {
+    	 	url: '/education',
+    	 	templateUrl: 'src/templates/education.html'
+    	 })
+
+    	 // experience part
+    	 .state('experience',{
+    	 	url: '/experience',
+    	 	templateUrl: 'src/templates/experience.html'
+    	 })
+
+    	 // project part
+    	 .state('project', {
+			url: '/project',
+    	 	templateUrl: 'src/templates/project.html'
+    	 })
+
+		// volunteer part
+    	 .state('volunteer',{
+    	 	url: '/volunteer',
+    	 	templateUrl: 'src/templates/volunteer.html'
+    	 });
+    }
+})();
