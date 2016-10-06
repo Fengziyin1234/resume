2ed1a095a6115a5fd72b5921b01bf2635256652f
Isaac Feng <zfeng5@stevens.edu>
2016-10-06 10:45:01 -0400

    shaking portrait

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,4 @@
+(function(doc, script) {
 // Create a clone of the menu, right next to original.
 $('.resume__header').addClass('original').clone().insertAfter('.resume__header').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
 
@@ -25,3 +26,24 @@ function stickIt() {
     $('.original').css('visibility','visible');
   }
 }
+
+  var js, 
+      fjs = doc.getElementsByTagName(script)[0],
+      frag = doc.createDocumentFragment(),
+      add = function(url, id) {
+          if (doc.getElementById(id)) {return;}
+          js = doc.createElement(script);
+          js.src = url;
+          id && (js.id = id);
+          frag.appendChild( js );
+      };
+      
+    // Google+ button
+    add('https://apis.google.com/js/plusone.js');
+    // Facebook SDK
+    add('//connect.facebook.net/en_US/all.js#xfbml=1&appId=200103733347528', 'facebook-jssdk');
+    // Twitter SDK
+    add('//platform.twitter.com/widgets.js');
+
+    fjs.parentNode.insertBefore(frag, fjs);
+}(document, 'script'));
